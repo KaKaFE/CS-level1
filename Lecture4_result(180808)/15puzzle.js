c99b5480202f0dec4c321780111ced0c11731d97
sk2504 <sk2504@naver.com>
2018-08-18 22:17:32 +0900

    15퍼즐
    뒤집기, 오른쪽,왼쪽 밀어내기 버튼 구현을 했다.

---
+++ Lecture4_result(180808)/15puzzle.js
@@ -0,0 +1,43 @@
+var game = {};
+var str = document.getElementById('word').innerHTML
+var word2 = document.getElementById('word2')
+
+game.word = [];
+game.str = str.split('')
+game.instr = function () {
+    for (i = 0; i < game.str.length; i++) {
+        game.word[i].innerHTML = game.str[i];
+    }
+};
+
+for (i = 0; i < str.length; i++) {
+    var btn = document.createElement('button');
+    btn.innerHTML = str[i];
+    word2.appendChild(btn);
+    game.word.push(btn);
+}
+
+function Reverse() {
+    var Rstr = [];
+    for (i = 0; i < game.word.length; i++) {
+        Rstr.push(game.str.pop())
+        game.word[i].innerHTML = Rstr.shift()
+    }
+     for (i = 0; i < game.word.length; i++) {
+        game.str.push(game.word[i].innerHTML);
+    }
+};
+function Rpush() {
+    var str = game.str.pop();
+    game.str.unshift(str)
+    game.instr();
+};
+
+function Lpush() {
+    var str = game.str.shift();
+    game.str.push(str)
+    game.instr();
+};
+function check(){
+   
+}
