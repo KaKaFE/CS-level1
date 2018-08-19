31ed00091d0b1b3ba04f6f883b7e9b69093bbc7f
sk2504 <sk2504@naver.com>
2018-08-19 18:23:21 +0900

    15퍼즐
    리팩토링을 했다, 변수이름짓기가 너무어렵다 ;
    그 객체가 하는일을 영어로 표현하는게 힘듬.
    주석을넣어서 설명을 추가했다

--- Lecture4_result(180808)/15puzzle.js
+++ Lecture4_result(180808)/15puzzle.js
@@ -1,65 +1,86 @@
+var word = document.getElementById('word'); // HTML element
+var word2 = document.getElementById('word2'); // HTML element
+var answer = document.getElementById('check');
+
+// game object
 var game = {};
-// var str = document.getElementById('word').innerHTML
-var word2 = document.getElementById('word2')
+game.btnword = []; // buttons
+game.ranword = ['culture', 'symbol', 'ocean', 'price', 'route', 'effort', 'section', 'solution', 'gift', 'glory'] // words
 
+// ranword 배열에서 랜덤한 1개의 단어를 뽑는 getword
+game.getword = function () {
+    var str = game.ranword[Math.floor(Math.random() * game.ranword.length)].toUpperCase();
+    game.nowWord = str.split('')
+};
+// 뽑은 단어를 word에 표시
+game.nowdisplay = function () {
+    word.innerHTML = game.nowWord.join('');
+};
 
-game.word = [];
-game.ranword = ['culture', 'symbol', 'ocean', 'price', 'route', 'effort', 'section', 'solution', 'gift', 'glory']
-var str = game.ranword[Math.floor(Math.random() * game.ranword.length)].toUpperCase();
-document.getElementById('word').innerHTML = str;
-game.str = str.split('')
-game.instr = function () {
-    for (i = 0; i < game.str.length; i++) {
-        game.word[i].innerHTML = game.str[i];
+// 버튼을생성, nowword 의값을 버튼에 1개씩 넣는다.
+game.btndisplay = function () {
+    for (i = 0; i < game.nowWord.length; i++) {
+        var btn = document.createElement('button');
+        btn.innerHTML = game.nowWord[i];
+        word2.appendChild(btn);
+        game.btnword.push(btn);
     }
 };
 
-for (i = 0; i < str.length; i++) {
-    var btn = document.createElement('button');
-    btn.innerHTML = str[i];
-    word2.appendChild(btn);
-    game.word.push(btn);
-}
-function shuffle() {
-    var n = Math.floor((Math.random() * str.length)+1)
+//  nowword의순서가 바뀌면, 바뀐순서의 배열값을 다시버튼에 넣는다.
+game.reDisplay = function () {
+    for (i = 0; i < this.nowWord.length; i++) {
+        this.btnword[i].innerHTML = this.nowWord[i];
+    }
+};
+
+//  버튼을 섞어준다,
+game.shuffle = function () {
+    var n = Math.floor((Math.random() * this.nowWord.length) + 1)
     for (i = 0; i < n; i++) {
         Rpush();
         Reverse();
         Lpush()
     }
-}
-shuffle();
+};
+game.getword();
+game.nowdisplay();
+game.btndisplay();
+game.shuffle();
 
+
+//  버튼 배열을 뒤집는 함수
 function Reverse() {
     var Rstr = [];
-    for (i = 0; i < game.word.length; i++) {
-        Rstr.push(game.str.pop())
-        game.word[i].innerHTML = Rstr.shift()
+    for (i = 0; i < game.btnword.length; i++) {
+        Rstr.push(game.nowWord.pop())
+        game.btnword[i].innerHTML = Rstr.shift()
     }
-    for (i = 0; i < game.word.length; i++) {
-        game.str.push(game.word[i].innerHTML);
+    for (i = 0; i < game.btnword.length; i++) {
+        game.nowWord.push(game.btnword[i].innerHTML);
     }
 };
+// 배열을 오른쪽으로 미는함수
 function Rpush() {
-    var str = game.str.pop();
-    game.str.unshift(str)
-    game.instr();
+    var str = game.nowWord.pop();
+    game.nowWord.unshift(str)
+    game.reDisplay();
 };
 
+//배열을 왼쪽으로 미는 함수
 function Lpush() {
-    var str = game.str.shift();
-    game.str.push(str)
-    game.instr();
+    var str = game.nowWord.shift();
+    game.nowWord.push(str)
+    game.reDisplay();
 };
 
+// 배열의 값이 정답과 맞는지 체크하는 함수
 function check() {
-    var a = ""
-    for (i = 0; i < game.word.length; i++) {
-        a += game.word[i].innerHTML
-    }
-    if (str === a) {
-        document.getElementById('check').innerHTML = '일치합니다'
+    var a = word.innerHTML
+    var b = game.nowWord.join('')
+    if (a === b) {
+        answer.innerHTML = '일치합니다'
     } else {
-        document.getElementById('check').innerHTML = '일치하지 않습니다'
+        answer.innerHTML = '일치하지 않습니다'
     }
 };
